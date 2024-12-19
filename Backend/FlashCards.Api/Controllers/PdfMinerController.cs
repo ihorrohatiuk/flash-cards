546ca1b173fc69c462f0a1d1182f7a495d4ad040
ihorrohatiuk <rogatyuk60605@gmail.com>
2024-12-19 12:42:00 +0100

    Add AiPdfMiner (not complete)

---
+++ Backend/FlashCards.Api/Controllers/PdfMinerController.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using FlashCards.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashCards.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PdfMinerController : ControllerBase
+{
+    private LLMService _llmService;
+    
+    [HttpPost]
+    public async Task<ActionResult> GetFlashCardsFromPdfFile([FromForm] IFormFile? file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("No file uploaded or the file is empty.");
+        
+        if (file.ContentType != "application/pdf")
+            return BadRequest("Only PDF files are allowed.");
+        
+        _llmService = new LLMService();
+        try
+        {
+            var tempPath = "C:\\Users\\rogat\\PERSONAL\\Bachaleurs work\\PdfExamples\\";
+            var filePath = Path.Combine(tempPath, file.FileName);
+
+            await using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            
+            var fileContent = _llmService.ParsPdfFile(filePath);
+            var result = _llmService.MakeFlashCardsFromPdfContent(fileContent);
+
+            return Ok(result.Result);
+        }
+        catch
+        {
+            return StatusCode(500, "An error occurred while uploading the file.");
+        }
+    }
+}
---
+++ Backend/FlashCards.Api/Services/LLMService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+
+namespace FlashCards.Api.Services;
+
+public class LLMService
+{
+    public LLMService() { }
+
+    //TODO: rewrite pdf parser to another class
+    public string ParsPdfFile(string filePath)
+    {
+        try
+        {
+            using PdfReader reader = new  PdfReader(filePath);
+            using PdfDocument pdfDoc = new PdfDocument(reader);
+            int numberOfPages = pdfDoc.GetNumberOfPages();
+
+            var sb = new StringBuilder();
+            for (int i = 1; i <= numberOfPages; i++)
+            {
+                Console.WriteLine($"Parsing {i}...");
+                string pageText = ParsePage(pdfDoc, i);
+                
+                sb.AppendLine(pageText);
+                //SavePageTextToFile(pageText, i, filePath + ".txt");
+            }
+            
+            return sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Parsing {filePath} failed", ex);
+        }
+    }
+    
+    private string ParsePage(PdfDocument pdfDoc, int pageNumber)
+    {
+        if (pageNumber < 1 || pageNumber > pdfDoc.GetNumberOfPages())
+            throw new ArgumentOutOfRangeException(nameof(pageNumber), "Incorrect page number.");
+        
+        return PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(pageNumber));
+    }
+    
+    private void SavePageTextToFile(string pageText, int pageNumber, string filePath)
+    {
+        File.AppendAllText(filePath, pageText, Encoding.UTF8);
+        Console.WriteLine($"Pdf text {pageNumber} saved in: {filePath}");
+    }
+    
+    // Ollama
+    public async Task<string> MakeFlashCardsFromPdfContent(string content)
+    {
+        string url = "http://localhost:11434/api/generate";
+        var prepromtInfo = "From this file content generate 10 flash cards in format <Qustion>:<Answer>. Respond using JSON. File content: ";
+        var jsonData = $@"
+                       {{
+                         ""model"": ""llama2"",
+                         ""prompt"":""{prepromtInfo + content}"",
+                         ""format"": ""json"",
+                         ""stream"": false
+                       }}";
+        
+        using HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Accept", "application/json");
+        HttpContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        
+        HttpResponseMessage response = await client.PostAsync(url, httpContent);
+        
+        return await response.Content.ReadAsStringAsync();
+    }
+}
