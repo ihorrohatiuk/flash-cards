b07f53d37db20ff6fa5b9d14d684cee4c58c17ec
ihorrohatiuk <rogatyuk60605@gmail.com>
2025-03-16 21:48:33 +0100

    Add validations for Units page, create mock data for unit page.

--- src/FlashCards.Infrastructure/Helpers/TextHelper.cs
+++ src/FlashCards.Infrastructure/Helpers/TextHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace FlashCards.Infrastructure.Helpers;
+﻿using System.Text;
+
+namespace FlashCards.Infrastructure.Helpers;
 
 public static class TextHelper
 {
@@ -9,5 +11,18 @@ public static class TextHelper
             return String.Empty;
         }
         return text.Length <= maxLength ? text : text.Substring(0, maxLength) + "...";
+    }
+
+    public static string GetFirstLettersOfName(string fullName)
+    {
+        string[] names = fullName.Split(" ");
+        var sb = new StringBuilder();
+        // Returning only 2 letters, even if name has more then 2 words
+        for (int i = 0; i < 2; i++)
+        {
+            sb.Append(names[i][0].ToString().ToUpper());
+        }
+        
+        return sb.ToString();
     }
 }
---
+++ src/FlashCards.WebUI/Mocks/FlashCardsUnitDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashCards.WebUI.Mocks;
+
+public class FlashCardsUnitDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public bool IsPrivate { get; set; }
+    public string Owner { get; set; } = string.Empty;
+    public float Progress { get; set; }
+    public int CardsQuantity { get; set; }
+}
