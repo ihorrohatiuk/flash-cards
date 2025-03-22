f271ec0daca73c485767cdda9c5cb87d6aa88821
ihorrohatiuk <rogatyuk60605@gmail.com>
2025-03-22 21:57:28 +0100

    Add flash card, 2 buttons and progress labels.

--- src/FlashCards.WebUI/Mocks/FlashCardsUnitInfoDto.cs
+++ src/FlashCards.WebUI/Mocks/FlashCardsUnitInfoDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace FlashCards.WebUI.Mocks;
 
-public class FlashCardsUnitDto
+public class FlashCardsUnitInfoDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
