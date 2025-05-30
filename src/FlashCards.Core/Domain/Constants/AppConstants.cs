50f924d8fc8926c264429b82bec58c3010dda6af
ihorrohatiuk <rogatyuk60605@gmail.com>
2025-05-30 17:36:21 +0200

    Make constant for maximum quantity of flashcards per one unit.

--- src/FlashCards.Core/Domain/Constants/AppConstants.cs
+++ src/FlashCards.Core/Domain/Constants/AppConstants.cs
@@ -6,4 +6,5 @@ public static class AppConstants
     public const int MaxQuestionLength = 300;
     public const int MaxAnswerLength = 300;
     public const int MaxUnitNameLength = 50;
+    public const int FlashCardsMaxQuantityPerUnit = 200;
 }
