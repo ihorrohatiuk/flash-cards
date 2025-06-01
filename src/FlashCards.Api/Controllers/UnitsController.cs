3b0a8e8400b7e9b5d07146cc103e41d811932414
ihorrohatiuk <rogatyuk60605@gmail.com>
2025-06-01 12:20:34 +0200

    Add /delete-unit method for Units controller on backend.

--- src/FlashCards.Api/Controllers/UnitsController.cs
+++ src/FlashCards.Api/Controllers/UnitsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using FlashCards.Core.Application.Dtos;
+using FlashCards.Core.Domain.Constants;
 using FlashCards.Core.Domain.Entities;
 using FlashCards.Infrastructure.Persistence.Contexts;
 using FlashCards.Infrastructure.Persistence.DataModels;
@@ -58,5 +59,37 @@ public class UnitsController : ControllerBase
         await _context.SaveChangesAsync();
 
         return Ok(new { Message = "Unit and flashcards added successfully", UnitId = unitId });
+    }
+    
+    [HttpPost("delete-unit")]
+    public async Task<IActionResult> DeleteUnit([FromQuery] Guid flashCardsUnitId)
+    {
+        // TODO: ALSO DELETE ALL FLASHCARDS in PROGRESS, SRS METHOD, THAT are linked to this unit!!! 
+
+        //Getting token from request
+        _ = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+        Guid userId = Guid.Parse(User.FindFirst(JwtClaims.UserId)?.Value);
+        string userRole = User.FindFirst(ClaimTypes.Role)?.Value;
+        
+        if (flashCardsUnitId == Guid.Empty)
+            return BadRequest("Invalid unit id.");
+
+        var unit = await _context.FlashCardsUnits.FindAsync(flashCardsUnitId);
+        if (unit == null)
+            return NotFound("Unit not found.");
+        if (unit.OwnerId != userId && !string.Equals(userRole, RolesType.Admin))
+            return Forbid();
+
+        var cards = _context.FlashCards
+            .Where(c => c.FlashCardsUnitId == flashCardsUnitId);
+
+        // Remove flashcards
+        _context.FlashCards.RemoveRange(cards);
+        
+        // Remove unit
+        _context.FlashCardsUnits.Remove(unit);
+        await _context.SaveChangesAsync();
+
+        return Ok(new { message = "Unit deleted successfully." });
     }
 }
